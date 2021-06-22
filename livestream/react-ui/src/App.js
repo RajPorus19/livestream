ab03996129180667b62717e974b7d9efb62c69eb
RajPorus19 <raj.porus.hiruthayaraj@gmail.com>
2021-06-22 12:31:24 +0200

    ObtainTokenPair in front end

--- livestream/react-ui/src/App.js
+++ livestream/react-ui/src/App.js
@@ -1,31 +1,36 @@
-import './App.css';
-import './video-js.css';
-import VideoPlayer from './components/VideoPlayer.js';
-import ChatBox from './components/chat/ChatBox.js';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import "./video-js.css";
+import VideoPlayer from "./components/VideoPlayer.js";
+import ChatBox from "./components/chat/ChatBox.js";
+import LoginForm from "./components/login/loginForm.js";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-
-    const videoJsOptions = {
-      autoplay: true,
-      controls: true,
-      sources: [{
-          src: 'https://stream.porus.dev/hls/show.m3u8',
-        type: 'application/x-mpegURL'
-      }]
-    }
+  const videoJsOptions = {
+    autoplay: true,
+    controls: true,
+    sources: [
+      {
+        src: "https://stream.porus.dev/hls/show.m3u8",
+        type: "application/x-mpegURL",
+      },
+    ],
+  };
   return (
     <div className="App">
-        <div class="container">
-            <div class="row">
-                <div class="col-8">
-                    <VideoPlayer { ...videoJsOptions } />
-                </div>
-                <div class="col-4">
-                    <ChatBox />
-                </div>
-            </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-8">
+            <VideoPlayer {...videoJsOptions} />
+          </div>
+          <div class="col-4">
+            <ChatBox />
+          </div>
+          <div class="col-4">
+            <LoginForm />
+          </div>
         </div>
+      </div>
     </div>
   );
 }
---
+++ livestream/react-ui/src/components/login/loginForm.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+export default class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+    };
+  }
+  mySubmitHandler = async (event) => {
+    event.preventDefault();
+    const res = await obtainTokenPair(this.state.username, this.state.password);
+    const access = res["access"];
+    const refresh = res["refresh"];
+  };
+  myChangeHandler = (event) => {
+    let nam = event.target.name;
+    let val = event.target.value;
+    this.setState({ [nam]: val });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.mySubmitHandler}>
+        <h1>Hello {this.state.username}</h1>
+        <p>Enter your username:</p>
+        <input type="text" name="username" onChange={this.myChangeHandler} />
+        <p>Enter your password:</p>
+        <input
+          type="password"
+          name="password"
+          onChange={this.myChangeHandler}
+        />
+        <br />
+        <br />
+        <input type="submit" />
+      </form>
+    );
+  }
+}
+
+async function obtainTokenPair(username, password) {
+  const rawResponse = await fetch("http://127.0.0.1:8000/api/token/", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: username,
+      password: password,
+    }),
+  });
+  const content = await rawResponse.json();
+  return content;
+}
