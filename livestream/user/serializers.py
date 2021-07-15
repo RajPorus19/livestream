2178940ca3556038221fee2c360c41c0a71ed85f
RajPorus19 <raj.porus.hiruthayaraj@gmail.com>
2021-07-15 12:04:15 +0200

    user fields modified to read onlh

--- livestream/user/serializers.py
+++ livestream/user/serializers.py
@@ -7,4 +7,12 @@ class UserSerializer(serializers.ModelSerializer):
         fields = "__all__"
         extra_kwargs = {
             'password': {'write_only': True},
+            'last_login': {'read_only': True},
+            'date_joined': {'read_only': True},
+            "is_superuser": {'read_only': True},
+            "is_staff": {'read_only': True},
+            "is_active": {'read_only': True},
+            "date_joined": {'read_only': True},
+            "groups": {'read_only': True},
+            "user_permissions": {'read_only': True},
         }
