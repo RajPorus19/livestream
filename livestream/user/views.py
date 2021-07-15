5db0bca6ff7d7b48034f08753280cc091756f096
RajPorus19 <raj.porus.hiruthayaraj@gmail.com>
2021-07-15 13:54:44 +0200

    Fixed error with password when creating a user from react app

--- livestream/user/views.py
+++ livestream/user/views.py
@@ -1,7 +1,13 @@
 from rest_framework import viewsets
 from .serializers import UserSerializer 
 from django.contrib.auth import get_user_model
+from rest_framework.response import Response
 
 class UserSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = get_user_model().objects.all()
+
+    def create(self, args, **kwargs):
+        user = get_user_model().objects.create_user(**self.request.data)
+        print(user)
+        return Response(data=None)
